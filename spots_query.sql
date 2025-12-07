5c959a6d2b6dc712335f84f6f642d79d9df963bf
Brandon Isabell <bisabell2006@gmail.com>
2025-12-07 13:02:57 -0500

    MySQL database

---
+++ spots_query.sql
@@ -0,0 +1,34 @@
+DROP SCHEMA IF EXISTS `EatingSPots`;
+CREATE SCHEMA `EatingSpots`;
+
+USE `EatingSpots`;
+
+SET NAMES utf8;
+SET character_set_client = utf8mb4;
+
+
+CREATE TABLE sign_up( 
+email VARCHAR(50) NOT NULL, 
+username VARCHAR(24) NOT NULL, 
+password VARCHAR(24) NOT NULL 
+);
+
+
+CREATE TABLE spot_rating( 
+id INT,  
+spot_name VARCHAR(50), 
+rating INT,
+favorite_dinning VARCHAR(50),
+favorite_study VARCHAR(50)
+);
+
+CREATE TABLE reviews(
+favorite_dinning VARCHAR(50),
+favorite_study VARCHAR(50)
+);
+
+
+
+
+
+
